53ee1d837238c623587b766742fe87c5e7f68046
milleja46 <milleja46@gmail.com>
2012-01-17 18:07:03 -0500

    some minor improvements and additions

--- luxobscura/main.py
+++ luxobscura/main.py
@@ -3,7 +3,7 @@
 import spyral
 import pygame as pg
 SIZE = (640, 640)
-BG_COLOR = (2, 255, 20)
+BG_COLOR = (0, 0, 0)
 colors = {}
 images = {}
 geom = {}
@@ -61,135 +61,146 @@ class Menu(spyral.scene.Scene):
         if self.inited:
             return
         self.inited = True
-        bg = spyral.util.new_surface(geom['size'])
-        bg.fill(colors['bg'])
+        self.active = 0
+        bg = pg.image.load("media/images/menu.png").convert()
         self.camera.set_background(bg)
-        self.menufont = pg.font.SysFont("None", 42)
+        self.menufont = pg.font.SysFont("None", int(.12*640))
+        self.menutitlefont = pg.font.SysFont("None", int(.20*640))
+        self.menucolor = (0, 200, 25)
         
-        title = spyral.sprite.Sprite()
-        title.image = images['menu_title']
-        title.rect.top = 10
-        title.rect.centerx = self.camera.get_rect().centerx
+        self.title = spyral.sprite.Sprite()
+        self.title.text = "Lux Obscura"
+        self.title.image = self.menutitlefont.render(self.title.text, True, self.menucolor)
+        self.title.rect.top = 10
+        self.title.rect.centerx = self.camera.get_rect().centerx
         
         self.newGame = spyral.sprite.Sprite()
-        self.newGame.text = "  New Game"
-        self.newGame.image = self.menufont.render(self.newGame.text, True, colors['menu'])
-        self.newGame.rect.top = title.rect.bottom + 40
-        self.newGame.rect.left = 190
+        self.newGame.text = "   New Game"
+        self.newGame.image = self.menufont.render(self.newGame.text, True, self.menucolor)
+        self.newGame.rect.top = self.title.rect.bottom + 70
+        self.newGame.rect.left = 180
 
         self.gameContinue = spyral.sprite.Sprite()
-        self.gameContinue.text = "  Continue"
-        self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, colors['menu'])
+        self.gameContinue.text = "   Continue"
+        self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, self.menucolor)
         self.gameContinue.rect.top = self.newGame.rect.bottom + 5
-        self.gameContinue.rect.left = 190
+        self.gameContinue.rect.left = 180
 
         self.options = spyral.sprite.Sprite()
-        self.options.image = images['menu_Options']
+        self.options.text = "   Options"
+        self.options.image = self.menufont.render(self.options.text, True, self.menucolor)
         self.options.rect.top = self.gameContinue.rect.bottom +5
-        self.options.rect.left = 190
+        self.options.rect.left = 180
 
         self.gameQuit =  spyral.sprite.Sprite()
-        self.gameQuit.image = images['menu_Quit']
+        self.gameQuit.text = "   Quit"
+        self.gameQuit.image = self.menufont.render(self.gameQuit.text, True, self.menucolor)
         self.gameQuit.rect.top = self.options.rect.bottom +5
-        self.gameQuit.rect.left = 190
+        self.gameQuit.rect.left = 180
 
         
-        self.group.add(title, self.newGame, self.gameContinue, self.options, self.gameQuit)
-        self.active = 0
+        self.group.add(self.title, self.newGame, self.gameContinue, self.options, self.gameQuit)
         
     def render(self):
         self.group.draw()
         spyral.director.get_camera().draw()
         if self.active == 0:
             self.newGame.text = "> New Game"
-            self.newGame.image = self.menufont.render(self.newGame.text, True, colors['menu'])
+            self.newGame.image = self.menufont.render(self.newGame.text, True, self.menucolor)
         if self.active != 0:
-            self.newGame.text = "  New Game"
-            self.newGame.image = self.menufont.render(self.newGame.text, True, colors['menu'])
+            self.newGame.text = "   New Game"
+            self.newGame.image = self.menufont.render(self.newGame.text, True, self.menucolor)
         if self.active == 1:
             self.gameContinue.text = "> Continue"
-            self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, colors['menu'])
+            self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, self.menucolor)
         if self.active != 1:
-            self.gameContinue.text = "  Continue"
-            self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, colors['menu'])
-            
-        strings['menu_Options'] = "Options"
-        strings['menu_Quit'] = "Quit"
-        self.group.update()
+            self.gameContinue.text = "   Continue"
+            self.gameContinue.image = self.menufont.render(self.gameContinue.text, True, self.menucolor)
+        if self.active == 2:
+            self.options.text = "> Options"
+            self.options.image = self.menufont.render(self.options.text, True, self.menucolor)
+        if self.active != 2:
+            self.options.text = "   Options"
+            self.options.image = self.menufont.render(self.options.text, True, self.menucolor)
+        if self.active == 3:
+            self.gameQuit.text = "> Quit"
+            self.gameQuit.image = self.menufont.render(self.gameQuit.text, True, self.menucolor)
+        if self.active != 3:
+            self.gameQuit.text = "   Quit"
+            self.gameQuit.image = self.menufont.render(self.gameQuit.text, True, self.menucolor)
 
     def update(self, tick):
-        global menu_active
-        self.active = menu_active
         for ev in pg.event.get():
             if ev.type == pg.KEYDOWN:
                 if ev.key == pg.K_UP:
-                    if menu_active < 0:
-                        menu_active = 4
+                    if self.active < 0:
+                        self.active = 3
                     else:
-                        menu_active =  menu_active - 1
-                if ev.key == pg.K_UP:
-                    if menu_active > 4:
-                        menu_active = 0
+                        self.active -= 1
+                if ev.key == pg.K_DOWN:
+                    if self.active > 3:
+                        self.active = 0
                     else:
-                        menu_active += 1
+                        self.active += 1
                 if ev.key == pg.K_RETURN:
                     if menu_active == 0:
-                        exit(0)
-                        spyral.director.push(intro)
+                        spyral.director.push(intro())
                     if menu_active == 1:
                         pass
                     if menu_active == 2:
                         pass
                     if menu_active == 3:
                         pass
-                    if menu_active == 4:
-                        exit(0)
             if ev.type == pg.QUIT:
                 exit(0)
-        
+        self.group.update()
+    def on_exit(self):
+        self.group.empty()
 class intro(spyral.scene.Scene):
-    def init(self):
-        spyral.Scene.__init__(self)
-    def on_exit(self):
-        pass
+    def __init__(self):
+        spyral.scene.Scene.__init__(self)
+        self.camera = spyral.director.get_camera().make_child(virtual_size=geom['size'])
+        self.group = spyral.sprite.Group(self.camera)
+        self.inited = False
+        
     def on_enter(self):
-        pass
+        if self.inited:
+            return
+        bg = spyral.util.new_surface(640, 640)
+        bg.fill(BG_COLOR)
+        self.camera.set_background(bg)
+        self.inited = True
+        self.player = spyral.sprite.Sprite()
+        self.player.image = pg.image.load("media/images/player.png")
+        self.player.rect.centerx = 320
+        self.player.rect.centery = 320
+
+        self.group.add(self.player)
     def render(self):
+        self.group.draw()
+        spyral.director.get_camera().draw()
+    def update(self, tick):
+        key = pg.key.get_pressed()
+        if key[pg.K_LEFT]:
+            self.player.rect.centerx -= 5
+        if key[pg.K_RIGHT]:
+            self.player.rect.centerx += 5
+        for ev in pg.event.get():
+            if ev.type == pg.QUIT:
+                exit(0)
+        self.group.update()
+class level(object):
+    def __init__(self):
         pass
-    def update(self):
+    def runLevel(self):
         pass
-class Playing(spyral.scene.Scene):
-    def init(self):
-        spyral.Scene.__init__(self)
-    def on_exit(self):
+    def getCurrent(self):
         pass
-    def on_enter(self):
+    def getNext(self):
         pass
-    def render(self):
+    def enemyObjects(self):
         pass
-    def update(self):
-        pass
-class Paused(spyral.scene.Scene):
-    def init(self):
-        spyral.Scene.__init__(self)
-    def on_exit(self):
-        pass
-    def on_enter(self):
-        pass
-    def render(self):
-        pass
-    def update(self):
-        pass
-class GameOver(spyral.scene.Scene):
-    def init(self):
-        spyral.Scene.__init__(self)
-    def on_exit(self):
-        pass
-    def on_enter(self):
-        pass
-    def render(self):
-        pass
-    def update(self):
+    def friendObjects(self):
         pass
 if __name__ == "__main__":
     spyral.init() # Always call spyral.init() first
@@ -199,33 +210,7 @@ if __name__ == "__main__":
     geom['size'] = SIZE
     geom['width'] =  640
     geom['height'] = 640
-    geom['menu_title_font_size'] = int(.20*geom['height'])
-    geom['menu_font_size'] = int(.12*geom['height'])
 
-    strings['menu_title']= "Lux Obscura"
-    strings['menu_newGame'] = "  New Game"
-    strings['menu_gameContinue'] = "  Continue"
-    strings['menu_Options'] = "  Options"
-    strings['menu_Quit'] = "  Quit"
-
-    fonts['menu'] = pg.font.SysFont(None, geom['menu_font_size'])
-    fonts['menu_title'] = pg.font.SysFont(None, geom['menu_title_font_size'])
-
-
-    images['menu_title'] = fonts['menu_title'].render(
-                            strings['menu_title'],
-                            True,
-                            colors['menu'])
-    images['menu_Options'] = fonts['menu'].render(
-                              strings['menu_Options'],
-                              True,
-                              colors['menu'])
-    images['menu_Quit'] = fonts['menu'].render(
-                            strings['menu_Quit'],
-                            True,
-                            colors['menu'])
-    
     spyral.director.init(SIZE) # the director is the manager for your scenes
-    spyral.director.push(Menu()) # push means that this Game() instance is
-                                 # on the stack to run
+    spyral.director.push(Menu()) # push means that this Game() instance is on the stack to run
     spyral.director.run() # This will run your game. It will not return.
